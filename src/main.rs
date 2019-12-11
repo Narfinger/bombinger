5a566ac8db6fcf713f92b207baf6fd50b9217219
Narfinger <narfinger@noreply.github.com>
2019-12-11 12:21:13 +0900

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+use anyhow::{Context, Result, anyhow};
 use chrono::{offset::TimeZone, NaiveDateTime};
 use chrono_tz::US::Pacific;
 use serde::{Deserialize, Serialize};
@@ -50,21 +52,21 @@ struct GiantBombVideo {
     pub video_show: Option<GiantBombVideoShow>,
 }
 
-pub fn from_giantbomb_datetime_to_timestamp(s: &str) -> i64 {
-    from_giantbomb_datetime(s).timestamp()
+pub fn from_giantbomb_datetime_to_timestamp(s: &str) -> Option<i64> {
+    from_giantbomb_datetime(s).map(|v| v.timestamp())
 }
 
-fn from_giantbomb_datetime(s: &str) -> chrono::DateTime<chrono::Utc> {
+fn from_giantbomb_datetime(s: &str) -> Option<chrono::DateTime<chrono::Utc>> {
     let dt = NaiveDateTime::parse_from_str(s, "%Y-%m-%d %H:%M:%S")
         .unwrap_or_else(|e| panic!("Error: {}", e));
     Pacific
         .from_local_datetime(&dt)
-        .unwrap()
-        .with_timezone(&chrono::Utc)
+        .map(|v| v.with_timezone(&chrono::Utc))
+        .single()
 }
 
 /// query giantbomb api and returns the result in a `GiantBombResult<T>`
-fn query_giantbomb<T>(t: &str, url: String) -> GiantBombResult<T>
+fn query_giantbomb<T>(t: &str, url: String) -> Result<GiantBombResult<T>>
 where
     T: serde::de::DeserializeOwned,
 {
@@ -74,25 +76,28 @@ where
     //println!("Query: {}", q);
     reqwest::get(q.as_str())
         .and_then(|mut s| s.json())
-        .unwrap_or_else(|e| panic!("error in json parsing: {}", e))
+        .context("Error in parsing json to get list of videos")
 }
 
 /// query the giantbomb videos, filter them according to `date` and returns them
-fn query_videos(config: &Config) -> Vec<GiantBombVideo> {
+fn query_videos(config: &Config) -> Result<Vec<GiantBombVideo>> {
     let qstring = VID_URL.to_string() + LIMIT;
-    let res = query_giantbomb(&config.gbkey, qstring).results;
-    res.into_iter()
+    let res = query_giantbomb(&config.gbkey, qstring).map(|v| v.results)?;
+    Ok(res
+        .into_iter()
         .filter(|v: &GiantBombVideo| !config.exclude.iter().any(|substr| v.name.contains(substr)))
         .filter(|v: &GiantBombVideo| {
-            chrono::Utc.timestamp(from_giantbomb_datetime_to_timestamp(&v.publish_date), 0)
-                > config.time
+            chrono::Utc.timestamp(
+                from_giantbomb_datetime_to_timestamp(&v.publish_date).expect("Timestamp error"),
+                0,
+            ) > config.time
         })
-        .collect()
+        .collect())
 }
 
-fn get_config() -> Config {
+fn get_config() -> Result<Config> {
     if let Ok(string) = fs::read_to_string("config.toml") {
-        toml::from_str(&string).expect("Error in reading config")
+        toml::from_str(&string).context("Error in reading config")
     } else {
         let c = Config {
             gbkey: "NOTHING".to_string(),
@@ -101,69 +106,80 @@ fn get_config() -> Config {
             exclude: Vec::new(),
             locked: false,
         };
-        write_config(&c);
+        write_config(&c)?;
         panic!("Please adjust config");
     }
 }
 
-fn write_config(c: &Config) {
-    let string = toml::to_string(c).expect("Error in serializing config");
-    fs::write("config.toml", string).expect("Error in writing config");
+fn write_config(c: &Config) -> Result<()> {
+    let string = toml::to_string(c).context("Error in serializing config")?;
+    fs::write("config.toml", string).context("Error in writing config")
 }
 
-fn download_video(config: &Config, vid: &GiantBombVideo) -> reqwest::Result<()> {
+fn download_video(config: &Config, vid: &GiantBombVideo) -> Result<()> {
     let mut path = config.path.clone();
 
-    let url = vid.hd_url.as_ref().unwrap().to_owned() + "?api_key=" + &config.gbkey;
-    //println!("Url: {}", url);
-    let mut response = reqwest::get(&url).expect("Could not find url");
+    let url = vid
+        .hd_url
+        .to_owned()
+        .map(|v| v + "?api_key=" + &config.gbkey);
 
-    path.push(format!(
-        "{}-{}-{}.mp4",
-        vid.publish_date,
-        &vid.video_show
-            .as_ref()
-            .map(|s| &s.title)
-            .unwrap_or(&"".to_string()),
-        vid.name.replace("/", "")
-    ));
+    if let Some(url) = url {
+        //println!("Url: {}", url);
+        let mut response = reqwest::get(&url).context("Could not find url")?;
 
-    println!("Downloading {:?}", path.to_str());
+        path.push(format!(
+            "{}-{}-{}.mp4",
+            vid.publish_date,
+            &vid.video_show
+                .as_ref()
+                .map(|s| &s.title)
+                .unwrap_or(&"".to_string()),
+            vid.name.replace("/", "")
+        ));
 
-    let mut dest = File::create(&path).expect("Could not do file");
-    copy(&mut response, &mut dest).expect("error in copy");
+        println!("Downloading {:?}", path.to_str());
+
+        let mut dest = File::create(&path).context("Could not do file")?;
+        copy(&mut response, &mut dest).context("error in copy")?;
+        Ok(())
+    } else {
+        Err(anyhow!("Could not read do url"))
+    }
+}
+
+fn run(config: &mut Config) -> Result<()> {
+    let videos = query_videos(config)?;
+    println!("Found {} new videos", videos.len());
+    for vid in videos {
+        download_video(config, &vid).with_context(|| format!("Error in video {}", vid.name))?;
+        config.time = from_giantbomb_datetime(&vid.publish_date).expect("Error in parsing time");
+        write_config(config)?;
+    }
     Ok(())
 }
 
 /// Update giantbomb videos and put them into the database
 pub fn main() {
-    let mut config = get_config();
-    if config.locked {
-        println!("Another instance is running (config file is locked). Aborting");
-        return;
-    }
-
-    config.locked = true;
-    write_config(&config);
-
-    let videos = query_videos(&config);
-    println!("Found {} new videos", videos.len());
-    for vid in videos {
-        let res = download_video(&config, &vid);
-        if res.is_err() {
-            println!(
-                "download failed of {}, {}",
-                &vid.name,
-                &vid.hd_url.unwrap_or_else(|| "None".to_string())
-            );
+    let config = get_config();
+    if let Ok(mut config) = config {
+        if config.locked {
+            println!("Another instance is running (config file is locked). Aborting");
+            return;
         }
 
-        config.time = from_giantbomb_datetime(&vid.publish_date);
-        write_config(&config);
-    }
+        config.locked = true;
+        write_config(&config).expect("Config in weird state, aborting");
+        if let Err(e) = run(&mut config) {
+            println!("Error in downloading");
+            println!("E: {}", e);
+        }
 
-    config.time = chrono::Utc::now();
-    config.locked = false;
-    write_config(&config);
-    println!("Finished downloading files");
+        config.time = chrono::Utc::now();
+        config.locked = false;
+        write_config(&config).expect("Error in writing config, possible corrupted");
+        println!("Finished downloading files");
+    } else {
+        println!("Config could not be loaded");
+    }
 }
